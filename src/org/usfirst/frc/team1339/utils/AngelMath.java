3c2d4ef25409053431d114db46188b69b62cf41a
Nate <nathanhaashoward@gmail.com>
2017-11-01 16:36:02 -0600

    Working on setting up GradleRIO

--- src/org/usfirst/frc/team1339/utils/AngelMath.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.team1339.utils;
-
-public class AngelMath {
-	public static double inchesToClicks(double in){
-		return (in * 256)/(3.95*Math.PI);
-	}
-}
--- web_resources/deploy.bat
+++
@@ -1,2 +0,0 @@
-pscp -pw "" %~dp0*.html %~dp0*.ico %~dp0*.js admin@roborio-1339-frc.local:/home/lvuser/web_resources
-pause
